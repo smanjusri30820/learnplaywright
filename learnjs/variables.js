f8251901dc533453e490812aeffb1f2e9d894c9d
smanjusri30820 <smnajusri30820@gmail.com>
2026-07-16 22:05:21 +0530

    adding some changes

---
+++ learnjs/variables.js
@@ -0,0 +1,16 @@
+// var studentname = "Manjusri"
+
+// console.log(studentname)
+
+
+// let studentname = "Manjusri"
+
+// studentname = "Manjusri S"
+
+// console.log(studentname)
+
+const studentname = "Manjusri"
+
+studentname = "Manjusri S"
+
+console.log(studentname)
